c89cea9adbbec59472f89d02e42b7455ac855c67
Samuel Čavoj <samuel@cavoj.net>
2022-05-29 23:08:03 +0200

    Generate random levels

--- levels.py
+++ levels.py
@@ -1,4 +1,6 @@
 from obstacle import Obstacle
+import constants as C
+from random import randint
 
 
 class Level:
@@ -12,3 +14,18 @@ levels = [
     Level([], 5, 60),
     Level([Obstacle(200, 200, 500, 500)], 10, 60),
 ]
+
+
+def generate_level(level):
+    if len(levels) > level:
+        return
+
+    obstacles = []
+    for i in range(level):
+        top = randint(0, C.HEIGHT)
+        left = randint(0, C.WIDTH)
+        w = randint(0, 200)
+        h = randint(0, 200)
+        obstacles.append(Obstacle(left, top, w, h))
+
+    levels.append(Level(obstacles, 9, 60))
